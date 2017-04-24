521b7c5368efd2b5b36045e507d9260604eb1433
Hai Pham <swiften.svc@gmail.com>
2017-04-25 03:06:45 +0800

    Added CellDequeType

--- SwiftUIUtilities/collection/UIListViewUtil.swift
+++ SwiftUIUtilities/collection/UIListViewUtil.swift
@@ -24,6 +24,7 @@ public extension CellIdentifierType {
 extension UICollectionViewCell: CellIdentifierType {}
 extension UITableViewCell: CellIdentifierType {}
 
+/// Implement this protocol to register cells.
 public protocol CellRegisterType {
     
     /// This method is common to both UICollectionView and UITableView.
@@ -32,6 +33,19 @@ public protocol CellRegisterType {
     ///   - cellClass: Any class object.
     ///   - identifier: A String value.
     func register(_ cellClass: AnyClass?, with identifier: String)
+}
+
+/// Implement this protocol to deque cells.
+public protocol CellDequeType {
+    associatedtype CellType: UIView
+    
+    /// Deque a cell with an identifier.
+    ///
+    /// - Parameters:
+    ///   - id: A String value.
+    ///   - at: An IndexPath instance.
+    /// - Returns: A CellType istance.
+    func dequeReusableCell(with id: String, for indexPath: IndexPath) -> CellType
 }
 
 public extension CellRegisterType {
@@ -44,14 +58,48 @@ public extension CellRegisterType {
     }
 }
 
+public extension CellDequeType {
+    
+    /// Deque a cell with a CellIdentifierType subclass.
+    ///
+    /// - Parameters:
+    ///   - type: A CellIdentifierType subclass type.
+    ///   - indexPath: An IndexPath instance.
+    /// - Returns: An optional CellIdentifierType instance.
+    func dequeReusableCell<C: CellIdentifierType>(with type: C.Type,
+                           for indexPath: IndexPath) -> C? {
+        return dequeReusableCell(with: type.identifier, for: indexPath) as? C
+    }
+}
+
 extension UICollectionView: CellRegisterType {
     public func register(_ cellClass: AnyClass?, with identifier: String) {
         register(cellClass, forCellWithReuseIdentifier: identifier)
     }
 }
 
+extension UICollectionView: CellDequeType {
+    public typealias CellType = UICollectionViewCell
+
+    public func dequeReusableCell(with id: String, for indexPath: IndexPath)
+        -> UICollectionViewCell
+    {
+        return dequeueReusableCell(withReuseIdentifier: id, for: indexPath)
+    }
+}
+
 extension UITableView: CellRegisterType {
     public func register(_ cellClass: AnyClass?, with identifier: String) {
         register(cellClass, forCellReuseIdentifier: identifier)
+    }
+}
+
+extension UITableView: CellDequeType {
+    public typealias CellType = UITableViewCell
+    
+    public func dequeReusableCell(with id: String, for indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        return dequeueReusableCell(withIdentifier: id, for: indexPath)
     }
 }
