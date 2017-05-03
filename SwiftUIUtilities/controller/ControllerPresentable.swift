12d9f2f5e1f4dc844f7096e402024a9a0afcbab0
Hai Pham <swiften.svc@gmail.com>
2017-05-03 14:56:45 +0800

    Changed ControllerPresentableType to be objc-compliant

--- SwiftUIUtilities/controller/ControllerPresentable.swift
+++ SwiftUIUtilities/controller/ControllerPresentable.swift
@@ -11,17 +11,36 @@ import UIKit
 /// Implement this protocol to present dialog controllers. We shall introduce
 /// a custom present(viewControllerToPresent:animated:completion:) method in
 /// order to simulate dialog appearance.
-public protocol ControllerPresentableType: class {
+@objc public protocol ControllerPresentableType: class {
     
-    /// Present a UIViewController instance.
+    /// Present a UIViewController instance. The method is different from
+    /// UIViewController.present(_:animated:completion:) because we want
+    /// to mark this method as @objc-compliant. If we use the same method
+    /// name and signatures, the compiler will complain.
     ///
     /// - Parameters:
     ///   - viewController: The UIViewController to be presented.
     ///   - animated: A Bool value.
     ///   - completion: Completion closure.
-    func present(_ viewController: UIViewController,
-                 animated: Bool,
-                 completion: (() -> Void)?)
+    @objc func presentController(_ controller: UIViewController,
+                                 animated: Bool,
+                                 completion: (() -> Void)?)
 }
 
-extension UIViewController: ControllerPresentableType {}
+extension UIViewController: ControllerPresentableType {
+    
+    /// Present a UIViewController instance. The method is different from
+    /// UIViewController.present(_:animated:completion:) because we want
+    /// to mark this method as @objc-compliant. If we use the same method
+    /// name and signatures, the compiler will complain.
+    ///
+    /// - Parameters:
+    ///   - viewController: The UIViewController to be presented.
+    ///   - animated: A Bool value.
+    ///   - completion: Completion closure.
+    @objc public func presentController(_ controller: UIViewController,
+                                        animated: Bool,
+                                        completion: (() -> Void)?) {
+        present(controller, animated: animated, completion: completion)
+    }
+}
